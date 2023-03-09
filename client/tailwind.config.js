3522c3a677be26b7b4d10517d8a9f77567e4b7fa
mfahriferdiansyah <xkadzuvic@gmail.com>
2023-03-09 11:24:33 +0700

    feat: Coins gacha integration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
